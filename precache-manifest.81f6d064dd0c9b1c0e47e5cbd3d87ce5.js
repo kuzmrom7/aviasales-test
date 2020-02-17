138700a72f7ba512620027ed4f060e77087ef02e
Kuzmrom7 <kuzmrom7@gmail.com>
2020-02-17 14:54:16 +0300

    Updates

---
+++ precache-manifest.81f6d064dd0c9b1c0e47e5cbd3d87ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97d3c155243c43735e3336e8bd00ace",
+    "url": "/aviasales-test/index.html"
+  },
+  {
+    "revision": "8ae3e0cb0357140d75ce",
+    "url": "/aviasales-test/static/css/main.d9a3f84b.chunk.css"
+  },
+  {
+    "revision": "abbbd6577900a5ac1579",
+    "url": "/aviasales-test/static/js/2.d98f5882.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/aviasales-test/static/js/2.d98f5882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae3e0cb0357140d75ce",
+    "url": "/aviasales-test/static/js/main.50b87d0d.chunk.js"
+  },
+  {
+    "revision": "b482dcf64305b78027e6",
+    "url": "/aviasales-test/static/js/runtime-main.f6bf16ac.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales-test/static/media/logo.0c089234.svg"
+  }
+]);
